c474ad58c78b6b0d0c94ba9743a99cec76ce2f45
pittalaranjeira <vitoria.pitta01@fatec.sp.gov.br>
2022-10-19 01:11:33 -0300

    Adição de Comentários Explicando o Código

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,6 +9,7 @@ export class HomePage {
 
   constructor() {}
 
+  //variabeis globais
   dtsistema
   dia
   semana
@@ -20,14 +21,15 @@ export class HomePage {
   vetor_semana = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado"]
   vetor_mes = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
   
+  //função para Buscar a Data do Sistema e manipular atráves das variáveis globais criadas acima
   BuscaData() {
-    this.dtsistema = new Date();
-    this.semana = this.dtsistema.getDay(); //dia da semana
-    this.dia = this.dtsistema.getDate(); //dia do mês
-    this.mes = this.dtsistema.getMonth(); //mês (a contagem inicia em Janeiro como mês 0)
+    this.dtsistema = new Date(); //data atual do sistema (Dia_da_Semana Mês Dia_mês Ano Horário Fuso-Horário)
+    this.semana = this.dtsistema.getDay(); //dia da semana (Dom à Sáb, inicia em Domingo como dia 0)
+    this.dia = this.dtsistema.getDate(); //dia do mês (1 a 30/31)
+    this.mes = this.dtsistema.getMonth(); //mês (Jan a Dez, inicia em Janeiro como mês 0)
     this.ano = this.dtsistema.getYear(); //ano (tem que somar 1900 na data)
-    this.semana_atual = this.vetor_semana[this.semana];
-    this.mes_atual = this.vetor_mes[this.mes];
-    this.ano_atual = this.ano + 1900;
+    this.semana_atual = this.vetor_semana[this.semana]; //atribui a semana_atual com auxílio do vetor_semana
+    this.mes_atual = this.vetor_mes[this.mes]; //atribui o mes_atual com auxílio do vetor_mes
+    this.ano_atual = this.ano + 1900; // por conta do bug do milênio, apagaram 1900 anos das datas
   }
 }
